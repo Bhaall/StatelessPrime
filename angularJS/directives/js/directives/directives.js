e00beafd630000b88efca74c8d34568e9785bda5
Rudy De Busscher <rdebusscher@gmail.com>
2013-03-29 09:30:05 +0100

    directives tutorial: programmatic widget

--- angularJS/directives/js/directives/directives.js
+++ angularJS/directives/js/directives/directives.js
@@ -1,5 +1,34 @@
 "use strict";
 
+var myPanel = {
+    create: function (element, options) {
+        element.addClass('pui-panel ui-widget ui-widget-content ui-corner-all')
+            .contents().wrap('<div class="pui-panel-content ui-widget-content" />');
+        var title = element.attr('title');
+
+        element.prepend('<div class="pui-panel-titlebar ui-widget-header ui-helper-clearfix ui-corner-all"><span class="ui-panel-title">'
+                + title + '</span></div>')
+            .removeAttr('title');
+
+    }
+};
+
+var myPanelLight = {
+    create: function (element, options) {
+        element.addClass('pui-panel ')
+            .contents().wrap('<div class="pui-panel-content" />');
+        var title = element.attr('title');
+
+        element.prepend('<div class="pui-panel-titlebar "><span class="ui-panel-title">'
+                + title + '</span></div>')
+            .removeAttr('title');
+
+    }
+
+
+
+};
+
 demo.directive('myDir1L', function version1() {
     return {
         restrict: 'EA'
@@ -15,6 +44,14 @@ demo.directive('myDir1L', function version1() {
             , template: '<div > <span >Panel title {{title}}</span><div ng-transclude></div></div>'
             , transclude: true
         }
+    }).directive('myDir3L', function version3() {
+        return {
+            restrict: 'A'
+            , compile: function (element, attrs) {
+                var options = {};
+                myPanelLight.create(element, options);
+            }
+        };
     });
 
 
@@ -35,6 +72,13 @@ demo.directive('myDir1', function version1() {
                 '<span class="ui-panel-title">Panel title {{title}}</span><div class="pui-panel-content ui-widget-content" ng-transclude></div></div>'
             , transclude: true
         }
+    }).directive('myDir3', function version3() {
+        return {
+            restrict: 'A'
+            , compile: function (element, attrs) {
+                var options = {};
+                myPanel.create(element, options);
+            }
+        };
     });
-
 
