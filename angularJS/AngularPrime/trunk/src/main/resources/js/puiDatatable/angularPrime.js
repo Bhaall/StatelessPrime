d94cd8159bdc681e030438f9b0ed15377c43fba2
Rudy De Busscher <rdebusscher@gmail.com>
2013-07-16 08:01:24 +0200

    Issue #12: datatable widget integration (first part)

---
+++ angularJS/AngularPrime/trunk/src/main/resources/js/puiDatatable/angularPrime.js
@@ -0,0 +1,105 @@
+"use strict";
+
+/*globals angular $ */
+
+angular.module('angular.prime').directive('puiDatatable', function () {
+    return {
+        restrict: 'A',
+        priority: 5,
+        compile: function (element, attrs) {
+            return function postLink(scope, element, attrs) {
+                var options = scope.$eval(attrs.puiDatatable) || {},
+                    data = [],
+                    functionBasedData = false,
+                    columns = element.data('puiColumns') || [],
+                    selectionMode = null,
+                    paginator = null;
+
+                if (angular.isArray(options)) {
+                    data = options;
+                }
+
+                if (angular.isFunction(options)) {
+                    data = options;
+                    functionBasedData = true;
+                }
+
+                if (angular.isArray(data) && data.length === 0) {
+                    if (angular.isFunction(options.tableData)) {
+                        functionBasedData = true;
+                    }
+                    data = options.tableData;
+
+                }
+
+                if (columns.length === 0) {
+                    if (options.columns) {
+                        columns = options.columns;
+                    }
+                    if (!functionBasedData && columns.length === 0) {
+                        for (var property in data[0]) {
+                            columns.push({field: property, headerText: property});
+                        }
+                    }
+                }
+
+                if (options.rowSelect) {
+                    selectionMode = 'single';
+                }
+
+                if (options.paginatorRows) {
+                    paginator = {
+                        rows: options.paginatorRows
+                    }
+                }
+
+                $(function () {
+
+                    element.puidatatable({
+                        caption: options.caption
+                        , datasource : data
+                        , columns: columns
+                        , selectionMode: selectionMode
+                        , rowSelect: options.rowSelect
+                        , paginator: paginator
+                    });
+
+                });
+
+            }
+        }
+    }
+});
+
+angular.module('angular.prime').directive('puiColumn', function () {
+    return {
+        restrict: 'A',
+        priority: 5,
+        compile: function (element, attrs) {
+            return function postLink(scope, element, attrs) {
+                  var columns = element.parent().data('puiColumns')
+                      , options = scope.$eval(attrs.puiColumn) || {}
+                      , columnInfo = {};
+
+                if (columns === undefined) {
+                    columns = [];
+                }
+
+                if (options.hasOwnProperty('field')) {
+                    columnInfo.field = options.field;
+                    columnInfo.headerText = options.headerText;
+                    if (columnInfo.headerText === undefined) {
+                        columnInfo.headerText = columnInfo.field;
+                    }
+                    columnInfo.sortable = options.sortable;
+
+                } else {
+                    columnInfo.field = attrs.puiColumn;
+                    columnInfo.headerText = attrs.puiColumn;
+                }
+                columns.push(columnInfo);
+                element.parent().data('puiColumns', columns);
+            }
+        }
+    }
+});
--- angularJS/AngularPrime/trunk/src/main/webapp/js/controllers.js
+++ angularJS/AngularPrime/trunk/src/main/webapp/js/controllers.js
@@ -132,6 +132,53 @@ function CheckBoxController($scope, Widgets) {
 
 }
 
+function DatatableController($scope, Widgets, puiGrowl) {
+
+    $scope.widgets = Widgets;
+
+    $scope.fixedData =  [
+        {'brand':'Volkswagen','year': 2012, 'color':'White', 'vin':'dsad231ff'},
+        {'brand':'Audi','year': 2011, 'color':'Black', 'vin':'gwregre345'},
+        {'brand':'Renault','year': 2005, 'color':'Gray', 'vin':'h354htr'},
+        {'brand':'Bmw','year': 2003, 'color':'Blue', 'vin':'j6w54qgh'},
+        {'brand':'Mercedes','year': 1995, 'color':'White', 'vin':'hrtwy34'},
+        {'brand':'Opel','year': 2005, 'color':'Black', 'vin':'jejtyj'},
+        {'brand':'Honda','year': 2012, 'color':'Yellow', 'vin':'g43gr'},
+        {'brand':'Chevrolet','year': 2013, 'color':'White', 'vin':'greg34'},
+        {'brand':'Opel','year': 2000, 'color':'Black', 'vin':'h54hw5'},
+        {'brand':'Mazda','year': 2013, 'color':'Red', 'vin':'245t2s'}
+    ];
+
+    $scope.remoteData = function (callback) {
+        $.ajax({
+            type: "GET",
+            url: 'json/cars.json',
+            dataType: "json",
+            context: this,
+            success: function (response) {
+                $scope.safeApply(  // external changes aren't picked up by angular
+                    callback.call(this, response)
+                )
+            }
+        });
+    };
+
+    $scope.carTableData = {
+        tableData : $scope.fixedData
+        , rowSelect: function(event, data) {
+            puiGrowl.showInfoMessage('Row selection', 'Selected a '+data.color+ ' '+data.brand+ ' of '+data.year +' (id = '+data.vin+')');
+        }
+    };
+
+    $scope.paginatedData = {
+        tableData : $scope.fixedData
+        , paginatorRows : 4
+        , rowSelect: function(event, data) {
+            puiGrowl.showInfoMessage('Row selection', 'Selected a '+data.color+ ' '+data.brand+ ' of '+data.year +' (id = '+data.vin+')');
+        }
+    }
+}
+
 function DialogController($scope, Widgets) {
 
     $scope.widgets = Widgets;
